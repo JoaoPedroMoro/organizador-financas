90f789ae481b7265591fcf01708b47e8a862981f
João Pedro Moro Bolognini <joaopedromorob@gmail.com>
2026-08-11 16:36:54 -0300

    Adicionando Spring Boot e Docker - config. inicial

---
+++ backend/src/main/java/br/com/organizador_financas/OrganizadorFinancasApplication.java
@@ -0,0 +1,13 @@
+package br.com.organizador_financas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorFinancasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/organizador_financas/OrganizadorFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.organizador_financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
