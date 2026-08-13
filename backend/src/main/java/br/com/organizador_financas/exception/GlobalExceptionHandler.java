94e05ada77c26855b4a7d55ed5831388fa5a4176
João Pedro Moro Bolognini <joaopedromorob@gmail.com>
2026-08-13 10:01:49 -0300

    Atualizando validações

--- backend/src/main/java/br/com/organizador_financas/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/br/com/organizador_financas/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package br.com.organizador_financas.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,6 +21,26 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+            MethodArgumentNotValidException exception) {
+
+        String mensagem = exception.getBindingResult()
+                .getFieldErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Bad Request",
+                mensagem
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(errorResponse);
     }
 }
